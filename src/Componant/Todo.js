a489b202c6156104b2c9d3d0dc07f42ad66c394d
aniket <aniketshendge50@gmail.com>
2024-12-04 19:02:32 +0530

    Initial commit or your commit message

---
+++ src/Componant/Todo.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+function Todo(props) {
+  const [inputText, setInputText] = useState('');
+
+  const handleAdd = () => {
+    if (inputText.trim() !== '') {
+      props.addList(inputText); // Adding inputText to the list
+      setInputText(''); // Clear input after adding
+    }
+  };
+
+  return (
+    <div className="input-container">
+      <input
+        type="text"
+        className="input-box-todo"
+        placeholder="Enter your Todo here"
+        value={inputText} // Controlled input
+        onChange={(e) => setInputText(e.target.value)} // Update inputText on change
+      />
+      <button className="add-but" onClick={handleAdd}>
+        +
+      </button>
+    </div>
+  );
+}
+
+export default Todo;
